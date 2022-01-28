c0ac35fd8d3d2cedd5a77eb7de7a91460caa6131
Oksana <kskaplun@gmail.com>
2022-01-28 23:35:49 +0200

    add 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '../css/common.css';
+import '../css/01-gallery.css';
 
-console.log(galleryItems);
+const createItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+      `;
+  })
+  .join('');
+
+const alleryContainerEl = document.querySelector('.gallery');
+alleryContainerEl.insertAdjacentHTML('beforeend', createItemsMarkup);
+
+alleryContainerEl.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    let lightbox = new SimpleLightbox('.gallery a', {
+      scrollZoom: false,
+      captionDelay: 250,
+      captionsData: 'alt',
+      doubleTapZoom: 1,
+    });
+  }
+});
+
+//console.log(galleryItems);
